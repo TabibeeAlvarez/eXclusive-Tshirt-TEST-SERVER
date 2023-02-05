b6ec795ce96ab6077af335a268b855c221a68e57
eXclusive Tshirt <116873249+TabibeeAlvarez@users.noreply.github.com>
2023-02-06 04:16:17 +0800

    Add files via upload

--- inside.js
+++ inside.js
@@ -10,16 +10,24 @@ const loob = document.getElementById("loob"),
         loob.classList.remove('show')
       })
 
-function navopen(){
-    document.querySelector('img').style.display = 'none'
-}
+// function navopen(){
+//     document.querySelector('img').style.display = 'none'
+// }
 
-function navclose(){
-    document.querySelector('img').style.display = 'block';
-}
+// function navclose(){
+//     document.querySelector('img').style.display = 'block';
+// }
 
 const img = document.getElementById('image1');
 
 img.addEventListener('load', () => {
   image1.classList.add('drop')
 })
+
+function shopnow(){
+  const button1 = document.getElementById('button1');
+
+  button1.addEventListener('click', () => {
+    window.location.href = 'https://www.facebook.com/Tabibeegaming'
+  })
+}
