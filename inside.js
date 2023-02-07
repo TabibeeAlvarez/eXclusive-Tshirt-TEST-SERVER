5aaf1bc7a8b2f22254228bb0b84a7a94f7512a93
eXclusive Tshirt <116873249+TabibeeAlvarez@users.noreply.github.com>
2023-02-07 17:56:49 +0800

    Add files via upload

--- inside.js
+++ inside.js
@@ -16,3 +16,21 @@ const img = document.getElementById('image1');
 img.addEventListener('load', () => {
   image1.classList.add('drop')
 })
+
+const welcome = document.querySelectorAll('.welcome');
+window.addEventListener('scroll', wels);
+
+function wels(){
+  const triggerBottom = innerHeight / 5 * 4;
+
+  welcome.forEach(wels => {
+    const welsTop = wels.getBoundingClientRect().top;
+
+    if(welsTop < triggerBottom){
+      wels.classList.add('welcomes')
+    }
+    else{
+      wels.classList.remove('welcomes')
+    }
+  })
+}
