0c3b006ecb873dcb204df3d57b6d01c4cb01d448
eXclusive Tshirt <116873249+TabibeeAlvarez@users.noreply.github.com>
2023-02-10 20:01:30 +0800

    Add files via upload

---
+++ design inside.js
@@ -0,0 +1,15 @@
+function dark(){
+    document.getElementById('ilaws').style.display = 'none'
+    document.getElementById('mains').style.backgroundColor = 'black'
+    document.getElementById('ilaw').style.display = 'block'
+    document.getElementById('back').style.color = 'white'
+    document.getElementById('pepe').style.color = 'white'
+}
+
+function light(){
+    document.getElementById('ilaws').style.display = 'block'
+    document.getElementById('mains').style.backgroundColor = 'white'
+    document.getElementById('ilaw').style.display = 'none'
+    document.getElementById('back').style.color = 'black'
+    document.getElementById('pepe').style.color = 'black'
+}
