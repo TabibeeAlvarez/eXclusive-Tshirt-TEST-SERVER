41039fd6104507f768694f6225705ba7261223ec
eXclusive Tshirt <116873249+TabibeeAlvarez@users.noreply.github.com>
2023-02-21 17:24:22 +0800

    Add files via upload

---
+++ inside.js
@@ -0,0 +1,17 @@
+// const list = document.querySelectorAll('.list')
+// window.addEventListener('click', lista)
+
+// function lista(){
+//     const triggerBottom = innerHeight / 5 * 4;
+
+//     list.forEach(lis => {
+//         const lisTop = lis.getBoundingClientRect().top;
+
+//         if(lisTop < triggerBottom){
+//             lis.classList.add('lista')
+//         }
+//         else{
+//             lis.classList.remove('lista')
+//         }
+//     })
+// }
